d04452e8df855472611ba13c1987e3b73a753459
tanveershah377 <tanveershah377@github.com>
2020-10-05 01:43:28 -0400

    forgot to upload this question 5 from week4day2

--- Homework_week4 day1and 2/src/day2/Q5_Average.java
+++ Homework_week4 day1and 2/src/day2/Q5_Average.java
@@ -3,8 +3,21 @@ package day2;
 public class Q5_Average {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int[] array= new int[5];
+		
+		array[0] = 10;
+		array[1]=20;
+        array[2] =30;
+        array[3]= 40;
+        array[4]= 50;
+        int sum=0;
+        
+        for (int t=0; t<array.length; t++) {
+        	
+        	sum= sum + array[t]/5;
+        }
+	System.out.println("average"+sum);
 	}
 
 }
